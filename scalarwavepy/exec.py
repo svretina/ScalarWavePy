976925dcac1f8c031791f8ecd7f018fdd1df8502
Stamatis Vretinaris <stamatis.vretinaris@aei.mpg.de>
2021-09-17 18:36:52 +0300

    applies linter

--- scalarwavepy/exec.py
+++ scalarwavepy/exec.py
@@ -6,22 +6,25 @@ import matplotlib.pyplot as plt
 import os
 from scipy.stats import norm
 
+
 def sine_pulse(x):
-    pulse = np.sin(16*np.pi*x)
-    pulse[x>0.125] = 0
+    pulse = np.sin(16 * np.pi * x)
+    pulse[x > 0.125] = 0
     return pulse
 
+
 def gaussian_pulse(x, x0=0.2):
-    rv = norm(loc = x0, scale = 0.05)
+    rv = norm(loc=x0, scale=0.05)
     pulse = rv.pdf(x)
     return pulse
 
-dx = 1/299
+
+dx = 1 / 299
 nx = 300
-dt = w.set_dt_from_courant(0.4,dx)
+dt = w.set_dt_from_courant(0.4, dx)
 nt = 600
 c = 1
-boundaries = [0,0]
+boundaries = [0, 0]
 pythran = False
 
 wave = w.ScalarWave(c, dx, dt, nx, nt)
@@ -44,9 +47,9 @@ t = wave.t
 
 for i in range(0, nt):
     # print(i,x[-1], len(x),len(sol[:,i]))
-    plt.plot(x,sol[:,i])
+    plt.plot(x, sol[:, i])
     # plt.ylim(-0.1,)
-    plt.xlim(x[0],x[-1])
+    plt.xlim(x[0], x[-1])
     plt.title(f"t={t[i]:.2f}")
     plt.savefig(f"./results/{i}.png")
     plt.clf()
--- scalarwavepy/wave.py
+++ scalarwavepy/wave.py
@@ -4,29 +4,29 @@ import numpy as np
 import scalarwavepy.pythran_lax as lax
 import matplotlib.pyplot as plt
 
-def set_dt_from_courant(c,dx):
-    return c*dx
+
+def set_dt_from_courant(c, dx):
+    return c * dx
 
 
 class ScalarWave:
-    def __init__(self, alpha, dx, dt, nx, nt, t0 = 0,x0 = 0):
-        """ Constructor of ScalarWave class.
-        """
-        self.c = alpha*dt/dx
+    def __init__(self, alpha, dx, dt, nx, nt, t0=0, x0=0):
+        """Constructor of ScalarWave class."""
+        self.c = alpha * dt / dx
         self.check_courant()
         self.nx = nx
         self.nt = nt
         self.x = self.discretize(x0, nx, dx)
         self.t = self.discretize(t0, nt, dt)
-        self.u = np.zeros((nx,nt)) # u(x,t)
-        self.ghost = np.zeros((2,nt))
-
+        self.u = np.zeros((nx, nt))  # u(x,t)
+        self.ghost = np.zeros((2, nt))
 
     def initialize_ghost_points(self):
-        self.ghost[0,0] = self.u[1,0]
-        self.ghost[1,0] = self.u[-2,0]
+        self.ghost[0, 0] = self.u[1, 0]
+        self.ghost[1, 0] = self.u[-2, 0]
 
-    def discretize(self, u0, nu, du):
+    @staticmethod
+    def discretize(u0, nu, du):
         uf = u0 + nu * du
         u = np.linspace(u0, uf, nu)
         return u
@@ -39,27 +39,41 @@ class ScalarWave:
         self.ghost[0, time_slice + 1] = self.u[1, time_slice + 1]
         self.ghost[1, time_slice + 1] = self.u[-2, time_slice + 1]
 
-    def calculate_boundaries(self, time_slice, boundary_type = "Neumann"):
+    def calculate_boundaries(self, time_slice, boundary_type="Neumann"):
         if boundary_type == "Neumann":
-            self.u[0, time_slice + 1] = self.lax_wendroff(self.ghost[0, time_slice], self.u[0, time_slice], self.u[1, time_slice], self.c)
-            self.u[-1, time_slice + 1] = self.lax_wendroff(self.ghost[1, time_slice], self.u[-1, time_slice], self.u[-2, time_slice], self.c)
+            self.u[0, time_slice + 1] = self.lax_wendroff(
+                self.ghost[0, time_slice],
+                self.u[0, time_slice],
+                self.u[1, time_slice],
+                self.c,
+            )
+            self.u[-1, time_slice + 1] = self.lax_wendroff(
+                self.ghost[1, time_slice],
+                self.u[-1, time_slice],
+                self.u[-2, time_slice],
+                self.c,
+            )
 
     def initialize_solution(self, func):
-        self.u[:,0] = func(self.x)
+        self.u[:, 0] = func(self.x)
 
     @staticmethod
     def lax_wendroff(um1, u, up1, c):
-        return u - ( c / 2 ) * ( up1 - um1 ) + (( c*c ) / 2. ) * ( up1 - 2 * u + um1 )
+        return (
+            u - (c / 2) * (up1 - um1) + ((c * c) / 2.0) * (up1 - 2 * u + um1)
+        )
 
     def solve_interior(self, time_slice):
-        for i in range(1, self.nx-1):
-            self.u[i, time_slice + 1] = self.lax_wendroff( self.u[i-1, time_slice],
-                                                           self.u[i, time_slice],
-                                                           self.u[i+1, time_slice],
-                                                           self.c)
+        for i in range(1, self.nx - 1):
+            self.u[i, time_slice + 1] = self.lax_wendroff(
+                self.u[i - 1, time_slice],
+                self.u[i, time_slice],
+                self.u[i + 1, time_slice],
+                self.c,
+            )
 
     def solve(self):
-        for j in range(0, self.nt-1):
+        for j in range(0, self.nt - 1):
             self.solve_interior(j)
             self.calculate_boundaries(j)
             self.calculate_ghost_points(j)
