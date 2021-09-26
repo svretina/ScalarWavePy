744095445fa5b673f08f7ab602d3a0a96e20d251
Stamatis Vretinaris <stamatis.vretinaris@aei.mpg.de>
2021-09-26 16:19:42 +0300

    adds rk2 ralston, rk4, enforces BCs in RHS function [Works]

--- scalarwavepy/ode.py
+++ scalarwavepy/ode.py
@@ -6,6 +6,7 @@ from scipy.stats import norm
 import matplotlib.pyplot as plt
 from scalarwavepy import mesh as m
 from scalarwavepy import wave as w
+import os
 
 def discretize(u0, ni, nu, du):
     ui = u0 + ni * du
@@ -28,11 +29,32 @@ def rk2(func, s, h, dx):
     s2 = s + h * k1
     return s2
 
+def rk2_ralston(func, s, h, dx):
+    k0 = func(s, dx)
+    s1 = s + (3/4)*h*k0
+    k1 = func(s1, dx)
+    s2 = s + h*((1/3)*k0+(2/3)*k1)
+    return s2
+
+def rk4(func, s, h, dx):
+    k0 = func(s, dx)
+    s1 = s + h/2 * k0
+    k1 = func(s1, dx)
+    s2 = s + h/2 * k1
+    k2 = func(s2, dx)
+    s3 = s + h * k2
+    k3 = func(s3, dx)
+    s3 = s + h/6 * (k0 + 2*k1 + 2*k2 + k3)
+    return s3
+
 def RHS(s, dx):
     u, pi, xi = s
-    # u[0] = 0
-    # u[-1] = 0
-    # xi = spatial_derivative(u, dx)
+    # enforce BCs
+    u[0] = 0
+    u[-1] = 0
+    pi[0] = 0
+    pi[-1] = 0
+
     dtu = pi
     dtpi = spatial_derivative(xi, dx)
     dtxi = spatial_derivative(pi, dx)
@@ -40,8 +62,8 @@ def RHS(s, dx):
 
 dx = 1/100
 dt = 0.4 * dx**(4/3)
-nx = 300
-nt = 400
+nx = 100
+nt = 2000
 
 u = np.zeros((nx, nt))
 pi = np.zeros((nx, nt))
@@ -58,14 +80,20 @@ xi[:,0] = spatial_derivative(u[:,0], dx)
 
 s = np.array([u[:,0], pi[:,0], xi[:,0]])
 
-for i in range(1, nt-1):
-    s = rk2(RHS, s, dt, dx)
+for i in range(1, nt):
+    s = rk4(RHS, s, dt, dx)
     u[:,i], _, _ = s
 
 for i in range(0, nt):
     plt.plot(x, u[:, i], "blue")
     plt.title(f"t={t[i]:.2f}")
-
+    plt.ylim(-8,8)
     plt.tight_layout()
     plt.savefig(f"./results/{i}.png")
     plt.clf()
+
+
+os.chdir("./results")
+print("Converting to gif...")
+os.system(f"convert -delay 0.5 -loop 0 {{0..{nt-1}}}.png wave.gif")
+os.system("mv wave.gif ../wave_rk4.gif")
